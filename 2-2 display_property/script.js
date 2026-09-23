57c1c6eeea0cd2fcd18ab3763dc94c3d92085044
Traci Vilez <tracivilez039@gmail.com>
2026-09-23 04:06:27 -0500

    javascript

---
+++ 2-2 display_property/script.js
@@ -0,0 +1,24 @@
+function resetDisplay() {
+    const productSelect =document.getElementById("contactType");
+    for (let i = 0; i < productSelect.options.length; i++) {
+        const productValue = productSelect.options[i].value;
+        if (productValue !== "") {
+            document.getElementById(productValue).classList.add("hidden");
+        };
+    };
+};
+
+
+function updateDisplay(selectedItem) {
+    if (selectedItem !== "") {
+        document.getElementById(selectedItem).classList.remove("hidden")
+    };
+};
+
+document.getElementById("contactType").addEventListener("change", (event) => {
+    
+    const selectedItem = event.currentTarget.value
+
+    resetDisplay(productValue);
+    updateDisplay(selectedItem);
+})
